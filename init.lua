52b3b9d19dde1a9f3bdc754b9e2b26ea7c3c592a
nikkischnelle <nikki@schnelle.dev>
2025-10-05 17:06:43 +0200

    add default lsps

--- init.lua
+++ init.lua
@@ -585,10 +585,19 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        clangd = {},
+        basedpyright = {},
+        ruff = {},
+        rust_analyzer = {},
+        jdtls = {},
+        bashls = {},
+        isort = {},
+        jq = {},
+        markdownlint = {},
+        prettierd = {},
+        yamllint = {},
+        yamlfmt = {},
+        xmlformatter = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
